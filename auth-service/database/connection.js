1b8ebf28d92d76ad802d770fefbcb50d491c08b0
AymeRx <aymeric.surre@gmail.com>
2024-01-09 10:48:20 +0100

    correction du bug qui empeché la connexion avec la bdd

--- auth-service/database/connection.js
+++ auth-service/database/connection.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'mysql-matthieu-73.alwaysdata.net',
+    user: '339794_aymeric',
+    password: '339794_aymeric_projet_dev_auth',
+    database: 'matthieu-73_integration_web'
 });
 
 connection.connect(error => {
